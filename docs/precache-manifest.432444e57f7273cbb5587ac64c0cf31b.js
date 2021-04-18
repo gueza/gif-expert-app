6b9845288094437dbdb20a3e995d03cf96bd6eba
Stephania Zambrano <stezague88@gmail.com>
2021-04-18 01:08:59 -0500

    docs agg

---
+++ docs/precache-manifest.432444e57f7273cbb5587ac64c0cf31b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c41e58a7d262c456ad76e7535ae8b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe4d120eba45555c17e",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe4d120eba45555c17e",
+    "url": "/static/js/main.b345c8b6.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
